a9efeb84cde1416eb968fb9863260e9951c64f4f
nathanmontt <contato.mont@gmail.com>
2025-06-06 22:11:15 -0300

    Término (praticamente 2) do HTML e CSS

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,2 +1,29 @@
 // o valor do elemento que eu clicar vai virar o valor de dentro do elemento do select an item
 'use strict'
+
+const buttonContainer = document.querySelector('.button-container');
+const dropdownOptions = document.getElementById('dropdown-options');
+
+buttonContainer.addEventListener('click', () => {
+    dropdownOptions.classList.toggle('hidden');
+})
+
+/*
+<body>
+    <div class="button-container">
+        <button id="open-dropdown">Select an item</button>
+        <img src="assets/img/down.png" alt="Dropdown Arrow" id="dropdown-arrow">
+    </div>
+    <div id="dropdown-options">
+        <div class="holder-text-img">
+            <p class="options">First Item</p>
+        </div>
+        <div class="holder-text-img">
+            <p class="options">Second Item</p>
+        </div>
+        <div class="holder-text-img">
+            <p class="options">Third Item</p>
+        </div>
+    </div>
+</body>
+*/
